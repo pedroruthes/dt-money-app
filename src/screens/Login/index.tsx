6116bacee9b34a4a5357ad5185cde13dd9cbd509
Pedro Henrique Camargo Ruthes <pedroo.ruthes@gmail.com>
2025-11-27 09:57:09 -0300

    feat: add useKeyboardVisible hook and AuthHeader component

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -2,11 +2,13 @@ import { View } from "react-native";
 
 import { DismissKeyboardView } from "@/components/DismissKeyboardView";
 import { LoginForm } from "./LoginForm";
+import { AuthHeader } from "@/components/AuthHeader";
 
 export const Login = () => {
   return (
     <DismissKeyboardView>
       <View className="flex-1 w-[82%] self-center">
+        <AuthHeader />
         <LoginForm />
       </View>
     </DismissKeyboardView>
